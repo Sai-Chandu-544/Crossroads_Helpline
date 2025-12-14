d084ad61ff3314971d5d7a5541b47142094e7b53
Sai-Chandu544 <pendotasaichandu1234@gmail.com>
2025-12-14 18:30:29 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: "/redux-crud-dashboard/",
+  base: "/Crossroads_Helpline.git",
 })
